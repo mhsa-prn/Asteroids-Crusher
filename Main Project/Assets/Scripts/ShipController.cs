09d3c30e20ea09defa6534c3cd7312058c81fbd8
Mahsa Piran <piran_mahsa@yahoo.com>
2022-12-15 14:39:06 +0330

    Define First Spaceship (Object)
    Define first spaceship and programming its moves

---
+++ Main Project/Assets/Scripts/ShipController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    #region Public Variables
+    public float speed;
+    Transform myTransform;
+    #endregion
+
+    #region Private Variables
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = myTransform.position.x;
+        Debug.Log(x);
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        myTransform.position += new Vector3(h, v, 0) * speed * Time.deltaTime;
+
+        myTransform.position = new Vector3(
+            Mathf.Clamp(myTransform.position.x, -9.045f, 9.045f),
+            Mathf.Clamp(myTransform.position.y, -4.21f, 3.23f),
+            myTransform.position.z
+    );
+
+    }
+    #endregion
+}
