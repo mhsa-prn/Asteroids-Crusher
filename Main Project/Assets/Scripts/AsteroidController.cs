6bd04d68dc9f829aa054460919730c2f8c86fff3
Mahsa Piran <piran_mahsa@yahoo.com>
2022-12-15 18:18:09 +0330

    Level 1 Asteroids Move

---
+++ Main Project/Assets/Scripts/AsteroidController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidController : MonoBehaviour
+{
+    #region Public Variables
+    public float speed;
+    public float rotationSpeed;
+    #endregion
+
+    #region Private Variables
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    private void Update()
+    {
+        transform.position += Vector3.down * speed * Time.deltaTime;
+        transform.Rotate(new Vector3(0, 0, 1) * rotationSpeed * Time.deltaTime);
+    }
+    #endregion
+}
---
+++ Main Project/Assets/Scripts/AsteroidsSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidsSpawner : MonoBehaviour
+{
+    #region Public Variables
+    public GameObject asteroidPrefab;
+    public Vector2 timeToSpawn;
+    public Vector2 xAxisLimitToSpawn;
+    #endregion
+
+    #region Private Variables
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(Random.Range(timeToSpawn.x, timeToSpawn.y));
+        Vector3 pos = transform.position;
+        pos.x = Random.Range(xAxisLimitToSpawn.x, xAxisLimitToSpawn.y);
+        Instantiate(asteroidPrefab, pos, Quaternion.identity);
+        StartCoroutine(Spawn());
+    }
+    #endregion
+}
