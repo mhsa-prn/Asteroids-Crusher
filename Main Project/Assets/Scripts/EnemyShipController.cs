6422dd7362c926b284d325554beae10a10b79803
Mahsa Piran <piran_mahsa@yahoo.com>
2022-12-16 19:23:39 +0330

    Define Enemy Spaceship and Its Moves

---
+++ Main Project/Assets/Scripts/EnemyShipController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShipController : MonoBehaviour
+{
+    #region Public Variables
+    public float vSpeed; //vertical speed
+    public float hSpeed; //Horizontal speed
+
+    #endregion
+
+    #region Private Variables
+    private int direction = 0; //1=> right   -1=> left    0=> no left no right
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    private void Start()
+    {
+        InvokeRepeating("ChangeDirection", 1, 0.5f);
+    }
+
+    private void Update()
+    {
+        Vector3 move = Vector3.down;
+        move.x = direction * hSpeed;
+        move.y = direction * vSpeed;
+        transform.position += move * Time.deltaTime;
+
+        //check spaceship out of bounds
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(transform.position.x, -8.48f, 8.48f);
+        transform.position = pos;
+    }
+
+    private void ChangeDirection()
+    {
+        direction = Random.Range(-1, 2);
+    }
+    #endregion
+}
